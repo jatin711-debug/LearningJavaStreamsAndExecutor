43a592d744a5b42b37fba5231ae8bf5eb47041c1
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2022-02-25 18:17:03 -0500

    2 commit

--- Streams.java
+++ Streams.java
@@ -1,17 +1,21 @@
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 
 class Streams{
     public static void main(String[] args){
         //var obj = Stream.of(List.of(1,2,3),List.of(4,5,6));
+
         //obj.flatMap(o->o.stream()).forEach(a->System.out.println(a));
 
 
         Predicate<Movies> predicate = m -> m.getId() > 1;
-        var movies = List.of(new Movies("Hello",1)
-                                ,new Movies("Ji",2)
-                                ,new Movies("Bolo",4));
+        var movies = List.of(new Movies("Hello",10)
+                                ,new Movies("Ji",20)
+                                ,new Movies("Hanji",20)
+                                ,new Movies("Bolo",40));
         //movies.stream().filter(predicate).forEach(a-> System.out.println(a.getId()));
 
         //movies.stream().limit(2).forEach(a-> System.out.println(a.getId()));
@@ -21,5 +25,27 @@ class Streams{
         //movies.stream().takeWhile(m -> m.getId() < 4 ).forEach(a-> System.out.println(a.getName()));
 
         //movies.stream().dropWhile(m -> m.getId() < 4 ).forEach(a-> System.out.println(a.getName()));
+
+        //movies.stream().sorted(Comparator.comparing(Movies::getName).reversed()).forEach(m -> System.out.println(m.getName()));
+
+        // movies.stream()
+        //     .map(Movies::getId)
+        //         .distinct()
+        //             .forEach(System.out::println);
+
+        
+    //    var res =  movies.stream()
+    //         .noneMatch(m -> m.getId() == 1 ); // allMatch() anyMatch() count() findFirst()->get() findAny()->get()
+    //         //max(<Comparator>) min(Campatator.comparing(Movies::getId))
+    //     System.out.println(res);
+
+
+    // Integer sum= movies.stream()
+    //     .map(Movies::getId)
+    //         .reduce(0,Integer::sum);
+    //     System.out.println(sum);
+
+
+    
     }
 }
